ebb05d12f4a00dbc32d8bc1b52e512b80be1e983
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2023-02-16 12:27:07 +0530

    :art: OnePlus link

--- amazon.js
+++ amazon.js
@@ -1,4 +1,3 @@
-const { resolve } = require("path");
 const puppeteer=require("puppeteer");
 const {email,password}=require("./secrets");
 
@@ -27,6 +26,61 @@ browserOpenPromise
     })
     .then(function(){
         console.log("Login clicked");
+        let emailTypePromise=waitAndType('input[id="ap_email"]',email);
+        return emailTypePromise;
+    })
+    .then(function(){
+        console.log("Email typed");
+        let clickContinuePromise=cTab.click('input[id="continue"]');
+        return clickContinuePromise;
+    })
+    .then(function(){
+        console.log("Continue clicked for email");
+        let passwordTypePromise=waitAndType('input[id="ap_password"]',password);
+        return passwordTypePromise;
+    })
+    .then(function(){
+        console.log("Password typed");
+        let clickContinuePromise=cTab.click('input[id="signInSubmit"]');
+        return clickContinuePromise;
+    })
+    .then(function(){
+        console.log("SignIn clicked");
+        let searchForMobilePromise=waitAndType('input[placeholder="Search Amazon"]',"Mobile Phones");
+        return searchForMobilePromise;
+    })
+    .then(function(){
+        console.log("Mobile Phones typed");
+        let pressEnterPromise=cTab.keyboard.press("Enter");
+        return pressEnterPromise;
+    })
+    .then(function(){
+        console.log("Enter pressed");
+        let wait3secPromise=cTab.waitForTimeout(3000);
+        return wait3secPromise;
+    })
+    .then(function(){
+        console.log("3 Second wait done");
+        function getOnePlusLink(){
+            let linksArr=[];
+            let links=document.querySelectorAll('li[id="p_89/OnePlus"] span .a-link-normal.s-navigation-item',{delay:1000});
+            for(let i=0;i<links.length;i++){
+                linksArr.push(links[i].getAttribute('href'));
+            }
+
+            return linksArr;
+        }
+        let clickOnePlusPromise=cTab.evaluate(getOnePlusLink);
+        return clickOnePlusPromise;
+    })
+    .then(function(linksArr){
+        console.log(linksArr);
+        let fullLink="https://www.amazon.com"+linksArr[0];
+        let visitLinkPromise=cTab.goto(fullLink);
+        return visitLinkPromise;
+    })
+    .then(function(){
+        console.log("OnePlus product visited");
     })
 
     function waitAndClick(selector){
@@ -35,6 +89,7 @@ browserOpenPromise
             waitForSelectorPromise
                 .then(function(){
                     let clickPromise=cTab.click(selector,{delay:50});
+                    return clickPromise;
                 })
                 .then(function(){
                     resolve();
@@ -46,3 +101,23 @@ browserOpenPromise
 
         return myPromise;
     }
+
+
+function waitAndType(selector,type){
+    let myPromise=new Promise(function(resolve,reject){
+        let waitForSelectorPromise=cTab.waitForSelector(selector);
+        waitForSelectorPromise
+            .then(function(){
+                let typePromise=cTab.type(selector,type,{delay:100});
+                return typePromise;
+            })
+            .then(function(){
+                resolve();
+            })
+            .catch(function(err){
+                reject(err);
+            })
+    })
+
+    return myPromise;
+}    
